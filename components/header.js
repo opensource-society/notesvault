ee48532a487cd51f007b581b7a7231ae9c8c0731
Harsh Ahuja <116972057+Harsh-26626@users.noreply.github.com>
2025-08-04 03:37:42 +0530

    Update header.js
    Updated path of header inside header.html

--- components/header.js
+++ components/header.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-        fetch('../components/header.html')
+        fetch('header.html')
         .then(res => res.text())
         .then(data => {
           document.getElementById('header-holder').innerHTML = data;
