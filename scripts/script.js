d117ce9b85ecbc73da2d0471946517d76544e4dd
Pranav Khalate <pranav.kh49@gmail.com>
2025-08-06 01:15:07 +0530

    Modified Dashboard Page
    - Enhanced overall styling of the page

--- scripts/script.js
+++ scripts/script.js
@@ -215,7 +215,7 @@ document.addEventListener('DOMContentLoaded', () => {
       // =============== Set Active Nav Link =============== //
       const currentPath = window.location.pathname.split('/').pop()
 
-      document.querySelectorAll('.nav-link').forEach((link) => {
+      document.querySelectorAll('.nav-link, .mobile-nav-link').forEach((link) => {
         const linkPath = link.getAttribute('href')?.split('/').pop()
         if (linkPath === currentPath) {
           link.classList.add('active')
