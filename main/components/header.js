71ab11d0f390e7674d555ec940b1cb861eb7dddd
adityai0 <adityai0@users.noreply.github.com>
2025-08-12 15:12:59 +0000

    deploy: 6f4b6ac61a0b3a3cb5e66aeec2aff172ab8a58c7

--- main/components/header.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-        fetch('../components/header.html')
-        .then(res => res.text())
-        .then(data => {
-          document.getElementById('header-holder').innerHTML = data;
-        });
-      });
