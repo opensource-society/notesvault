e377566885600060f5640c9cb6589c84d7473d85
Karunya-3 <karunyaaravapalli@gmail.com>
2025-09-02 19:02:28 +0530

    upload button on dashboard redirecting success

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,1 +1,8 @@
 // Student Dashboard (JavaScript)
+function redirectToUploadNotes() {
+    // Redirect to uploadnotes page in the same folder
+    window.location.href = 'upload.html';
+}
+
+// Add event listener to the button
+document.getElementById('upload-note-btn').addEventListener('click', redirectToUploadNotes);
